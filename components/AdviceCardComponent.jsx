1c633b56b4bd0c0b4514100a926ab298f200493c
Kass-S <kassiespacek101@gmail.com>
2025-02-28 12:22:59 -0800

    added fetch

--- components/AdviceCardComponent.jsx
+++ components/AdviceCardComponent.jsx
@@ -1,16 +1,31 @@
 import { Card } from "flowbite-react"
+import { GetAdvice } from "../src/service/service"
+import { useState, useEffect } from "react"
 
 const AdviceCardComponent = () => {
+    const [advice, setAdvice] = useState([]);
+    const [idNumber, setIdNumber] = useState([]);
 
+
+    const fetchAdvice = async () => {
+        let data = await GetAdvice();
+        setAdvice(data.slip.advice);
+        setIdNumber(data.slip.id);
+    }
+    fetchAdvice();
+
+    // useEffect(() => {
+    //     fetchAdvice();
+    // }, [])
 
     return(
         <div className="flex justify-center m-4">
-            <Card className="max-w-sm mx-4 card-bg-color">
+            <Card className="min-w-96 max-w-sm mx-4 card-bg-color">
                 <h5 className=" tracking-tight text-color-number text-center">
-                    Advice
+                    ADVICE #{idNumber}
                 </h5>
-                <p className="font-normal text-color-advice text-center text-2xl">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                <p className="text-color-advice text-center text-2xl font-extrabold">
+                    "{advice}"
                 </p>
                 
 
