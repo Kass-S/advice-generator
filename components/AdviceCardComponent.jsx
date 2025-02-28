3f033ae64741e057631213473dc9d4a09659683d
Kass-S <kassiespacek101@gmail.com>
2025-02-28 15:23:59 -0800

    help

--- components/AdviceCardComponent.jsx
+++ components/AdviceCardComponent.jsx
@@ -1,6 +1,6 @@
 import { Card } from "flowbite-react"
 import { GetAdvice } from "../src/service/service"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import diceIcon from "../src/assets/icon-dice.svg";
 import mobileDivider from "../src/assets/pattern-divider-mobile.svg";
 import desktopDivider from "../src/assets/pattern-divider-desktop.svg";
@@ -17,34 +17,26 @@ const AdviceCardComponent = () => {
     }
     fetchAdvice();
 
-    useEffect(() => {
-        fetchAdvice();
-    }, [advice])
 
-    return(
-        <div className="m-4">
-            <Card className="min-w-96 max-w-sm mx-4 card-bg-color p-2 pb-4">
-                <h5 className=" tracking-tight text-color-number text-center">
+    return (
+        <div className="m-4 ">
+            <Card className="min-w-96 max-w-sm mx-4 card-bg-color p-2 pb-4 relative">
+                <h5 className="tracking-tight text-color-number text-center">
                     ADVICE #{idNumber}
                 </h5>
                 <p className="text-color-advice text-center text-2xl font-extrabold">
                     "{advice}"
                 </p>
-
+    
                 <img className="my-2 sm:hiddden" src={mobileDivider} alt="pattern divider" />
                 <img className="hidden sm:block sm:my-2" src={desktopDivider} alt="pattern divider" />
-
-                
-
             </Card>
-            <div className="flex justify-center relative ">
-                <button className="bg-color-number rounded-full p-4 hover:shadow-lg hover:cursor-pointer -top-5" onClick={() => {
-                    fetchAdvice();
-                }}>
+            
+            <div className="flex justify-center absolute bottom-1 left-1/2 transform -translate-x-1/2">
+                <button className="bg-color-number rounded-full p-4 hover:shadow-lg hover:cursor-pointer" onClick={fetchAdvice}>
                     <img className="" src={diceIcon} alt="dice icon" />
                 </button>
             </div>
-            
         </div>
     )
 }
